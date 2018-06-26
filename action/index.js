9ad48785173f18ae13b9ab14f100b958fc33421b
stanleyHuang <s703702002@gmail.com>
2018-06-26 22:28:21 +0800

    add redux react router

---
+++ action/index.js
@@ -0,0 +1,4 @@
+const login = {
+  type: 'login',
+};
+
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,81 +1,76 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-let ip = require('ip');
+const ip = require('ip');
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        app: './components/root.js'
-    },
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, './dist'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$|\.scss$/,
-                use: [
-                    {
-                        loader: 'style-loader'
-                    },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: true,
-                            localIdentName: '[local]'
-                        }
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            plugins: function () {
+  mode: 'development',
+  entry: {
+    app: './components/root.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, './dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$|\.scss$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              localIdentName: '[local]',
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins () {
                                 return [
                                     require('autoprefixer')
                                 ];
-                            }
-                        }
-                    },
-                    {
-                        loader: 'sass-loader'
-                    }
-                ]
+                            },
             },
-            {
-                test: /\.js?$|\.jsx?$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-                loader: 'file-loader?name=fonts/[name].[ext]'
-            }
-        ]
-    },
-    devServer: {
-        contentBase: path.join(__dirname, './'),
-        compress: true,
-        port: 9000,
-        host: ip.address()
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            inject: true,
-            template: './index.html',
-        }),
-        new webpack.HotModuleReplacementPlugin()
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+      {
+        test: /\.js?$|\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        loader: 'file-loader?name=fonts/[name].[ext]',
+      },
     ],
-    // externals: {
-    //     'react': 'React',
-    //     'react-dom': 'ReactDOM',
-    //     'moment': 'moment',
-    // },
-    resolve: {
-        alias: {
-            Root: path.resolve(__dirname)
-        }
-    }
+  },
+  devServer: {
+    contentBase: path.join(__dirname, './'),
+    compress: true,
+    port: 9000,
+    host: ip.address(),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './index.html',
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  // externals: {
+  //     'react': 'React',
+  //     'react-dom': 'ReactDOM',
+  //     'moment': 'moment',
+  // },
 };
