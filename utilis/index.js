adbb24e79a1caab42e217dcf79f986a7695fd9b0
stanleyHuang <s703702002@gmail.com>
2018-07-30 20:38:44 +0800

    add sort

--- utilis/index.js
+++ utilis/index.js
@@ -1,2 +1,9 @@
 export const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 export { default } from './click_outside';
+
+function serialize(obj) {
+  const str = Object.entries(obj).map(item => `${encodeURIComponent(item[0])}=${encodeURIComponent(item[1])}`);
+  return str.join('&');
+}
+
+export { serialize };
