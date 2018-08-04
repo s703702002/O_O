80b91489829e2256b7391083e2f85ee99b39bf05
StanleyHuang <s703702002@gmail.com>
2018-08-04 17:54:28 +0800

    update

--- containers/ShoppingCart.js
+++ containers/ShoppingCart.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const Product = ({ shoppings }) => (
+const Product = ({ shoppingCart }) => (
   <div>
     <h4>我的購物車:</h4>
     {
-      shoppings.map(item => (
+      !shoppingCart.length ?
+      '購物車裡面沒東西唷' :
+      shoppingCart.map(item => (
         <div
           className="in_shopping_cart"
           key={item.id}
@@ -19,24 +21,27 @@ const Product = ({ shoppings }) => (
   </div>
 );
 
-const SoppingCart = ({ status, shoppings }) => (
+const SoppingCart = ({ status, shoppingCart }) => (
   <div className="shopping_cart mr-2">
     <i className="material-icons md-24">shopping_cart</i>
     <div className="cart_content box-shadow">
       {
         (status === 'init' || status === 'loginerr') ?
           '請先登入!' :
-          <Product shoppings={shoppings} />
+          <Product shoppingCart={shoppingCart} />
       }
     </div>
   </div>
 );
 
 const mapStateToProps = (state, ownProps) => {
-  const { login: { status, shoppings } } = state;
+  const {
+    shoppingCart,
+    login: { status },
+  } = state;
   return {
     status,
-    shoppings,
+    shoppingCart,
   };
 };
 
--- reducers/login.js
+++ reducers/login.js
@@ -1,48 +1,34 @@
-import { clone } from '../utilis';
-
 const login = (state = { status: 'init' }, action) => {
-  const newShoppings = !state.shoppings ? null : clone(state.shoppings);
   switch (action.type) {
     case 'LOGIN_REQUEST':
       return {
         status: 'loading',
         username: null,
         message: null,
-        shoppings: [],
       };
     case 'LOGIN_SUCCESS':
       return {
         status: 'logined',
         username: action.response.name,
         message: null,
-        shoppings: action.response.shoppings,
       };
     case 'LOGIN_ERROR':
       return {
         status: 'loginerr',
         username: null,
         message: action.error,
-        shoppings: [],
       };
     case 'LOGIN_CANCEL':
       return {
         state: 'init',
         username: null,
         message: null,
-        shoppings: [],
       };
     case 'LOG_OUT':
       return {
         status: 'init',
         username: null,
         message: null,
-        shoppings: [],
-      };
-    case 'ADD_TO_CART':
-      newShoppings.push(action.product);
-      return {
-        ...state,
-        shoppings: newShoppings,
       };
     default:
       return state;
