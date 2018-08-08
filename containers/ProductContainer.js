8046d7f7ed41d7280bc9c1a03f1f10f50a0897d0
StanleyHuang <s703702002@gmail.com>
2018-08-08 17:58:30 +0800

    update

--- containers/ProductContainer.js
+++ containers/ProductContainer.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import cx from 'classnames';
-import { getProductRequset } from '../action'; 
+import { getProductRequset } from '../action';
 
+const LoadProduct = () => (
+  <p>正在載入產品</p>
+);
 class ProductContainer extends Component {
   componentDidMount() {
     const { dispatch } = this.props;
@@ -11,11 +14,12 @@ class ProductContainer extends Component {
     dispatch(getProductRequset(productId));
   }
   render() {
+    console.log(this.props.product);
     return (
       <div className="container">
         <div className="row justify-content-center">
           <div className="col-10">
-            One of three columns
+            <LoadProduct />
           </div>
         </div>
       </div>
