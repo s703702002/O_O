adf2a832fe9b54327249945cac8d63c4937009d6
StanleyHuang <s703702002@gmail.com>
2018-08-17 18:03:22 +0800

    update addtocart saga

--- sagas/addToCartSaga.js
+++ sagas/addToCartSaga.js
@@ -1,23 +1,14 @@
-import { takeEvery, put, select, call, all } from 'redux-saga/effects';
+import { takeEvery, put, select, call, take } from 'redux-saga/effects';
 import { delay } from '../utilis';
 import {
-  openLoginBox,
   addToCart,
   showAddFinished,
   hiddenAddFinished,
   addLightBoxMessage,
-  removeLightBoxMessage,
+  openLoginBox,
 } from '../action';
 
 const getLoginStatus = state => state.login.status;
-
-function* openBox() {
-  yield put(openLoginBox);
-}
-
-function* watchOpenBox() {
-  yield takeEvery(removeLightBoxMessage(), openBox);
-}
 
 function* addToCartFlow(action) {
   try {
@@ -26,7 +17,9 @@ function* addToCartFlow(action) {
     // if haven't login
     if (loginStatus === 'init' || loginStatus === 'loginerr') {
       yield put(addLightBoxMessage('請先登入'));
-      console.log('action', action);
+      // 等 user 關閉lightbox
+      yield take('REMOVE_LIGHT_BOX_MESSAGE');
+      yield put(openLoginBox);
     } else {
       yield put(addToCart(product, count));
       yield put(showAddFinished);
@@ -35,6 +28,8 @@ function* addToCartFlow(action) {
     }
   } catch (err) {
     // do something
+  } finally {
+    // always do
   }
 }
 
