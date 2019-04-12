84c0228b0bfb8777409b6e248025f55d06d91b73
zhuang01 <zhaoyuan.huang@oath.com>
2019-04-12 17:08:18 +0800

    update

--- src/utilis/util.spec.js
+++ src/utilis/util.spec.js
@@ -1,4 +1,7 @@
-import { getRandomItem } from './index'
+import {
+  getRandomItem,
+  queryToObj,
+} from './index'
 
 test('getRandomItem test', () => {
   const arr = [1, 2, 3, 4, 5, 6, 7];
@@ -9,20 +12,23 @@ test('getRandomItem test', () => {
   randomItem.forEach(v => {
     expect(arr.includes(v)).toBeTruthy();
   });
-})
+});
 
 test('getRandomItem err', () => {
   const arr = [];
   const amount = 3;
   const randomItem = getRandomItem(arr, amount);
 
-  expect(randomItem).toEqual(null);
-})
+  expect(randomItem).toBeNull();
+});
 
-test('故意錯', () => {
-  const arr = [1, 2, 3, 4, 5, 6, 7];
-  const amount = 3;
-  const randomItem = getRandomItem(arr, amount);
-
-  expect(randomItem.length).toEqual(4);
+test('queryToObj', () => {
+  const mockQuery = '?a=a&b=b&c=c';
+  const queryObj = queryToObj(mockQuery);
+  
+  expect(queryObj).toBeInstanceOf(Object);
+  expect(queryObj.a).toBe('a');
+  expect(queryObj.b).toBe('b');
+  expect(queryObj.c).toBe('c');
+  expect(queryObj.foo).toBeUndefined();
 })
